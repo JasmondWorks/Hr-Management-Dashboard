d9f2c607d744c61b9aae50644559b32cb8b5164e
Obafemi Olorede <obafemilared@gmail.com>
2024-07-12 12:44:53 +0100

    update

--- server/index.js
+++ server/index.js
@@ -1,18 +1,25 @@
-const jsonServer = require("json-server");
-const cors = require("cors");
-const path = require("path");
+// const jsonServer = require("json-server");
+// const cors = require("cors");
+// const path = require("path");
 
-const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, "db.json"));
-const middlewares = jsonServer.defaults();
+// const server = jsonServer.create();
+// const router = jsonServer.router(path.join(__dirname, "db.json"));
+// const middlewares = jsonServer.defaults();
 
-server.use(cors());
-server.use(jsonServer.bodyParser);
-server.use(middlewares);
-server.use(router);
+// server.use(cors());
+// server.use(jsonServer.bodyParser);
+// server.use(middlewares);
+// server.use(router);
 
-const PORT = 8000;
+// const PORT = 8000;
 
-server.listen(PORT, () => {
-  console.log(`JSON Server is running on http://localhost:${PORT}`);
-});
+// server.listen(PORT, () => {
+//   console.log(`JSON Server is running on http://localhost:${PORT}`);
+// });
+
+const express = require("express");
+const app = express();
+
+app.use("/", (req, res) => res.send("Server is running"));
+
+app.listen(5000, console.log("Server started on PORT 5000"));
